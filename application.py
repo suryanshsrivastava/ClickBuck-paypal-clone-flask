e0cc422927b6f87d22203a16f6188591aec44b07
suryanshsrivastava99 <suryansh.srivastava99@gmail.com>
2018-04-13 20:45:20 +0530

    Started

--- application.py
+++ application.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, flash, url_for
+# from flask_login import current_user, login_user
+from forms import LoginForm
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def main():
-    if authenticated
-        return redirect()
-    return render_template("index.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/user/')
+    return render_template('index.html', form=form)
 
 
 @app.route("/user/<username>")
