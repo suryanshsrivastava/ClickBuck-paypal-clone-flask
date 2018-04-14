28f93b4d29057f52e92d1386f9a2338305eeb488
suryanshsrivastava99 <suryansh.srivastava99@gmail.com>
2018-04-14 18:46:04 +0530

    Flask part working without login

--- application.py
+++ application.py
@@ -22,8 +22,7 @@ def user():
     if session.get("money") is None:
         session["money"] = 0
     if request.method == "POST":
-        add_money = int(request.form.get("add"))
-        session["money"] += add_money
+        session["money"] += int(request.form.get("add"))
     return render_template("user.html", money=session["money"])
 
 
